466a6a884fa933320ef56a51d6da72d5c2355494
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2021-04-20 12:53:37 +0100

    Create ShowBitsDemo.java

---
+++ src/com/jimboidin/chapter5/ShowBitsDemo.java
@@ -0,0 +1,18 @@
+package com.jimboidin.chapter5;
+
+public class ShowBitsDemo {
+    //Prints the binary version of byte 'val'
+    public static void main(String[] args){
+        int t;
+        byte val;
+        val = 123;
+
+        for (t=128; t > 0; t = t/2){
+            if ((val & t) != 0) System.out.print("1 ");
+            else System.out.print("0 ");
+        }
+
+    }
+
+}
+
