b5251a36c58325c5505d136b20da8b5b1e7d3b8f
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2021-04-27 11:09:53 +0100

    Update NotDemo.java
    Remove colon from print statements.

--- src/com/jimboidin/chapter5/NotDemo.java
+++ src/com/jimboidin/chapter5/NotDemo.java
@@ -14,8 +14,8 @@ public class NotDemo {
         // reverse all bits
         b = (byte) ~b;
         for (int t = 128; t > 0; t = t/2){
-            if ((b & t) != 0) System.out.print("1: ");
-            else System.out.print("0: ");
+            if ((b & t) != 0) System.out.print("1 ");
+            else System.out.print("0 ");
         }
     }
 }
