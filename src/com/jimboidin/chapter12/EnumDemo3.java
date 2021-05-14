6c4b79803b63544db6509375e5e9999c07626ed2
jamesclackett <55019466+jamesclackett@users.noreply.github.com>
2021-05-14 12:09:45 +0100

    Complete EnumDemo3

--- src/com/jimboidin/chapter12/EnumDemo3.java
+++ src/com/jimboidin/chapter12/EnumDemo3.java
@@ -1,4 +1,29 @@
 package com.jimboidin.chapter12;
 
+enum Transport {
+    CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(70), BOAT(22);
+    private int speed;
+
+    Transport(int speed) {
+        this.speed = speed;
+    }
+
+    int getSpeed(){ return speed; }
+}
+
+
 public class EnumDemo3 {
+    public static void main(String[] args){
+        Transport tp;
+
+        // Display the speed of an airplane
+        System.out.println("Typical speed for an airplane is " + Transport.AIRPLANE.getSpeed() +
+                " miles per hour.\n");
+
+        // Display all Transports and speeds
+        System.out.println("All transport speeds:");
+        for (Transport t : Transport.values())
+            System.out.println(t + " typical speed is " + t.getSpeed() + " miles " +
+                    "per hour.");
+    }
 }
--- src/com/jimboidin/test/Test.java
+++ src/com/jimboidin/test/Test.java
@@ -1,14 +1,6 @@
 package com.jimboidin.test;
 
 
-enum Transport {
-    CAR(65), PLANE(20), TRUCK(20);
-
-    Transport(int speed) {
-    }
-}
-
-
 public class Test {
     public static void main(String[] args) throws InterruptedException {
 
